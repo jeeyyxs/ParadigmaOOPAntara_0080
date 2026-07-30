bd2d449b68b30de9477d6742bb91fb569c2be86e
jeeyyxs <riodwimulyono0512@gmail.com>
2026-07-30 14:29:47 +0700

    menambahkan variabel dan fungsi input data

--- contohinput.cpp
+++ contohinput.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class Barang{
+    public:
+        string nama;
+        int jumlah;
+        string kategori;
+        string tahunProduksi;
+
+        void inputData(){
+            cout << "=========== Input Data ===========" << endl;
+            cout << "Masukkan Nama Barang: ";
+            cin >> nama;
+            cout << "Masukkan Jumlah Barang: ";
+            cin >> jumlah;
+            cout << "Masukkan Kategori Barang: ";
+            cin >> kategori;
+            cout << "Masukkan Tanggal Produksi Barang: ";
+            cin >> tahunProduksi;
+        }
+    
+}
